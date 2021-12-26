ad79857dd96ebaa7babcb1738dfc02010466f178
Rexios <rexios@rexios.dev>
2021-12-25 23:31:40 -0500

    Forgot to export it

--- lib/pub_stats_core.dart
+++ lib/pub_stats_core.dart
@@ -1,5 +1,11 @@
 library pub_stats_core;
 
+// constant
+export 'src/constant/database_constants.dart';
+
+// model
 export 'src/model/global_stats.dart';
 export 'src/model/mini_package_score.dart';
+
+// service
 export 'src/service/user_agent_client.dart';
