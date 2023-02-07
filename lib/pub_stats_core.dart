62b84c7913eb4d77c6de4b15e9c5ed0e205570a4
Rexios <rexios@rexios.dev>
2023-02-07 12:32:35 -0500

    Actually export that

--- lib/pub_stats_core.dart
+++ lib/pub_stats_core.dart
@@ -4,6 +4,7 @@ library pub_stats_core;
 export 'src/constant/database_constants.dart';
 
 // model
+export 'src/model/developer_package_data.dart';
 export 'src/model/global_stats.dart';
 export 'src/model/mini_package_score.dart';
 
